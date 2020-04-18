14a0964fe2aa2c44c628ef9d033cdea3f4468170
Aland <aland.chang@smoothstack.com>
2020-04-18 17:48:49 -0400

    changed return type and junit

--- src/main/java/GetAgencies.java
+++ src/main/java/GetAgencies.java
@@ -5,19 +5,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
 
 import entity.Agency;
+import proxy.ApiGatewayProxyResponse;
+import proxy.ApiGatewayRequest;
 import util.ConnectUtil;
 
-public class GetAgencies implements RequestHandler<Map<String, String>, Object> {
+public class GetAgencies implements RequestHandler<ApiGatewayRequest, ApiGatewayProxyResponse> {
 
-	public Object handleRequest(Map<String, String> event, Context context) {
+	public ApiGatewayProxyResponse handleRequest(ApiGatewayRequest request, Context context) {
 		List<Agency> agencies = new ArrayList<Agency>();
 		Connection connection = null;
+		LambdaLogger logger = context.getLogger();
 		try {
 			connection = ConnectUtil.getInstance().getConnection();
 			PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Agency");
@@ -30,9 +34,12 @@ public class GetAgencies implements RequestHandler<Map<String, String>, Object> 
 				agency.setPhone(rs.getString("agencyPhone"));
 				agencies.add(agency);
 			}
-		} catch (ClassNotFoundException | SQLException e) {
-			e.printStackTrace();
+		} catch (SQLException e) {
+			return new ApiGatewayProxyResponse(400, null, null);
+		} catch (ClassNotFoundException e) {
+			logger.log(e.getMessage());
+			return new ApiGatewayProxyResponse(500, null, null);
 		}
-		return agencies;
+		return new ApiGatewayProxyResponse(200, null, new Gson().toJson(agencies));
 	}
 }
--- src/test/java/GetAgenciesTests.java
+++ src/test/java/GetAgenciesTests.java
@@ -1,18 +1,33 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import entity.Agency;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+import proxy.ApiGatewayProxyResponse;
+import proxy.ApiGatewayRequest;
 
 public class GetAgenciesTests {
+
+	private static Context context = null;
+	private static LambdaLogger logger = null;
+
+	@BeforeAll
+	public static void beforeAll() {
+		context = Mockito.mock(Context.class);
+		logger = Mockito.mock(LambdaLogger.class);
+		Mockito.when(context.getLogger()).thenReturn(logger);
+	}
 
 	@Test
 	public void getAgencies() {
 		GetAgencies getAgencies = new GetAgencies();
-		List<Agency> agencies = (List<Agency>) getAgencies.handleRequest(null, null);
-		assertFalse(agencies.isEmpty());
+		ApiGatewayRequest request = Mockito.mock(ApiGatewayRequest.class);
+		ApiGatewayProxyResponse response = getAgencies.handleRequest(request, context);
+		assertEquals(200, response.getStatusCode());
 	}
 
 }
